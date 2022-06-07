f0597e54bca1e55d994926e4a896a529900970cf
khendimike <khendimike@gmail.com>
2022-06-07 06:31:54 -0700

    function _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*main - Entry point
+*
+*Return: Always 0
+*/
+
+int main(void)
+{
+char s[] = "_putchar";
+int i = 0;
+
+while (i < 9)
+{
+_putchar(s[i]);
+i++;
+}
+
+return (0);
+}
