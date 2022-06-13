3b7a63c37cac9fa1ffb863372ceb887131e6f03f
khendimike <khendimike@gmail.com>
2022-06-13 07:23:43 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_puts - prints a string
+*@str: pointer variable parameter
+*/
+
+void _puts(char *str)
+{
+int i, j;
+
+while (*(str + i) != 0)
+{
+i++;
+}
+
+for (j = 0; j < i; j++)
+{
+_putchar(str[j]);
+}
+
+_putchar('\n');
+}
