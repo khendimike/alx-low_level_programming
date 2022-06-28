73ac4586f8e35c6bc7df45e85380523dd56dbf79
khendimike <khendimike@gmail.com>
2022-06-28 11:00:09 -0700

    function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,31 +1,57 @@
+#include "main.h"
 #include <stdlib.h>
-#include <main.h>
 /**
- * _strdup - copy
- * @str: pointer to string
- * Return: pointer
+ *_strdup-produces a copy of the string given as a parameter.
+ *@str:string to produced copy.
+ *Return:a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
  */
 
 char *_strdup(char *str)
 {
-char *s;
-int i = 0, j;
+int str_size;
+static char *duplicate;
+char *dup_offset;
 
-if (!str)
-return (NULL);
-while (*(str + i))
-i++;
-i++;
-s = malloc(sizeof(char) * i);
-
-if (s == NULL)
+if (str == NULL)
 {
 return (NULL);
 }
-
-for (j = 0; j <= i; j++)
+/* Allocate memory for duplicate */
+str_size = _strlen(str);
+duplicate = (char *)malloc(sizeof(char) * str_size + 1);
+if (duplicate == NULL)
 {
-s[j] = str[j];
+return ((char *)NULL);
 }
-return (s);
+/* Copy string */
+dup_offset = duplicate;
+while (*str)
+{
+*dup_offset = *str;
+dup_offset++;
+str++;
+}
+*dup_offset = '\0';
+
+return (duplicate);
+}
+
+
+/**
+ *_strlen-Finds the length of a string.
+ *@s:String pointer to the string whose length is to be found.
+ *
+ *Return: returns the length of the string.
+ */
+
+int _strlen(char *s)
+{
+int p = 0;
+/*incremeant up to when the last character is NULL,\0*/
+while (*(s + p) != 0)
+{
+p++;
+}
+return (p);
 }
