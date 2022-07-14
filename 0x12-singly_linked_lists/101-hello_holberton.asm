5a7795b2629b8234d7068a1c77eef4eba2c57f56
khendimike <khendimike@gmail.com>
2022-07-14 12:13:40 -0700

    Advanced tasks

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,9 @@
-section .text
-	global main
-	extern printf
-
-
-section .data
-	format db	"%s", 10, 0
-	message db	"Hello, Holberton", 0 ;Oxa-represents newline(\n).
-
+global    main
+          extern    printf
 main:
-	push	rbp		;points to the base of th current stack frame.
-	;; rbp is the register base pointer.This instruction will push the
-	;; value of rbp into the stack
-	mov	rsi, message	;rsi register source index.
-	;; passes, passess second argument to functions. it is the
-	;; register destination index.
-	mov	rdi, format
-	call 	printf
-	pop	rbp		;register base pointer.
-	ret			;Transfer program execution back to main program
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
