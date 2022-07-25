04cd6028df331f6f4a5c2aaa14ed05601a33ef85
khendimike <khendimike@gmail.com>
2022-07-25 11:54:41 -0700

    function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,43 @@
 #include "main.h"
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
+ * read_textfile -reads a text file and prints it to the POSIX standard output.
+ * @filename: file to be read.
  * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * Return:numbers it could read and print
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i, y;
-	char *buf;
-	if (!filename)
+	int fd, fd_write, fd_read;
+	char *buffer;
+
+	if (filename == NULL)
+	{
 		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
+	if (fd == -1)
 	{
-		free(buf);
 		return (0);
 	}
-	buf[i] = '\0';
+	fd_read = read(fd, buffer, letters);
+	if (fd_read == -1)
+	{
+		return (0);
+	}
+	fd_write = write(STDOUT_FILENO, buffer, fd_read);
+	if (fd_write == -1)
+	{
+		return (0);
+	}
+	free(buffer);
 	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	free(buf);
-	return (y);
+	return (fd_write);
 }
